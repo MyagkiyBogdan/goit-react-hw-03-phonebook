7315a73240d6855bf075435e76d9e10639800e6e
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-05-30 18:23:49 +0300

    localStorage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,32 @@ import ContactList from './ContactList';
 import EmptyMessage from './EmptyMessage';
 import Filter from './Filter';
 export class App extends Component {
+  INITIAL_STATE = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [...this.INITIAL_STATE],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    // if (parsedContacts !== null) if localStorage empty
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   deleteContact = contactId => {
     this.setState(prevState => ({
--- src/components/EmptyMessage/EmptyMessage.jsx
+++ src/components/EmptyMessage/EmptyMessage.jsx
@@ -1,7 +1,7 @@
 import styles from './EmptyMessage.module.css';
 
 const EmptyMessage = () => (
-  <p className={styles.text}>Your contact list is empty</p>
+  <p className={styles.text}>Your contacts list is empty</p>
 );
 
 export default EmptyMessage;
